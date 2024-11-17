502da91032dc8ab4789708eb57df390b1c3b4c62
Fathin Afif <fathinafiff@gmail.com>
2024-11-17 21:51:09 +0700

    add running script

--- app/main.py
+++ app/main.py
@@ -82,3 +82,9 @@ app.openapi = custom_get_openapi
 @app.on_event("startup")
 async def startup_event():
     Base.metadata.create_all(bind=engine)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
